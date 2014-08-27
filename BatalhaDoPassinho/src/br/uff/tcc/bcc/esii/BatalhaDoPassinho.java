f520b10abbf88c8eec0f9c116e1c91ea2e03ed7c
Matheus Corrêa <matheuscorrea@id.uff.br>
2014-08-27 09:06:54 -0300

    Exemplo de Commit: Data da Atualização: 27/08/2014 Descrição da Atualização: -Adição de mensagem de boas-vindas

--- BatalhaDoPassinho/src/br/uff/tcc/bcc/esii/BatalhaDoPassinho.java
+++ BatalhaDoPassinho/src/br/uff/tcc/bcc/esii/BatalhaDoPassinho.java
@@ -4,7 +4,7 @@ public class BatalhaDoPassinho {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Bem-vindo a Batalha do Passinho");
 	}
 
 }
