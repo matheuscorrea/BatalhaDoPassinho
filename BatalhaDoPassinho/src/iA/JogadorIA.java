8ffaad5a0a1eb2349b59524018f7529c1e3c2e19
Thadeu Jose <thadeujs@gmail.com>
2014-10-29 17:04:43 -0200

    Teste da implementação da IA
    Branch criado para fazer um brainstorm da melhor maneira de pensar a IA

---
+++ BatalhaDoPassinho/src/iA/JogadorIA.java
@@ -0,0 +1,24 @@
+package iA;
+
+import br.uff.tcc.bcc.esii.controlador.ControladorJogo;
+import br.uff.tcc.bcc.esii.modelo.Jogador;
+import br.uff.tcc.bcc.esii.visao.ConstanteDaCor;
+
+public class JogadorIA extends Jogador {
+	
+	public JogadorIA(String nome, ConstanteDaCor cor) {
+		super(nome, cor);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void fase1(){}
+	
+	public void fase2(){}
+	
+	public void fase3(){}
+	
+	public void rodada0(){}
+	
+	public void iniciaPartida(){}
+	
+}
