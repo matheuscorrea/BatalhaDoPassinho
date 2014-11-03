b2aec18d62115d56168f4916fbf860930f5fe813
AlCamp <al_campello@yahoo.com.br>
2014-11-02 23:45:37 -0200

    Correção

---
+++ BatalhaDoPassinho/src/br/uff/tcc/bcc/esii/BatalhaDoPassinho.java
@@ -0,0 +1,25 @@
+package br.uff.tcc.bcc.esii;
+
+import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.stage.Stage;
+import br.uff.tcc.bcc.esii.visao.GerenciadorDeTelas;
+import br.uff.tcc.bcc.esii.visao.GerenciadorDeTelas.TipoDaTela;
+
+public class BatalhaDoPassinho extends Application {
+
+	public static void inicia(String ... args){
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		GerenciadorDeTelas.getInstancia().setPrimaryStage(stage);
+ 		GerenciadorDeTelas.getInstancia().mudaTela(TipoDaTela.INICIO);
+ 		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		inicia();
+	}
+}
