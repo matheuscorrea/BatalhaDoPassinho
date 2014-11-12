52d460497070d1dbbe1b8302555c42c53611e824
Thadeu Jose <thadeujs@gmail.com>
2014-11-12 20:35:24 -0200

    Inicio dos Testes
    Inicio da bateria de testes

---
+++ BatalhaDoPassinho/teste/br/uff/tcc/bcc/esii/controlador/ControladorJogoTest.java
@@ -0,0 +1,239 @@
+package br.uff.tcc.bcc.esii.controlador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControladorJogoTest {
+
+	@Test
+	public void testAcaoTerritorio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistribuiTropa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFaseUmTemTropasParaDistribuir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMover() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProximaFase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIniciaPartida() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMapa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJogadores() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoAtaque() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFimDeJogo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoMover() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDominarTerritorio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJogadorDaVez() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminaJogador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoTelaTroca() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoTelaObjetivo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVoltaAoJogo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumeroCartasJogador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelecionouCartaTiro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelecionouCartaPorrada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelecionouCartaBomba() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelecionouCartaValesca() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCartasSelecionadas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoRealizaTroca() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObjetivoDoJogadorAtual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoSair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListaDeBotoesTerritorios() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJaMoveuTerritorio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLimpaBotoesFase3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionaJogador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoSalvar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcaoCarregar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecomecaAtributos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJogadorVencedor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAcabouJogo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGanharJogo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizaTela() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTropasADitribuir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetQuantidadeDeTropas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJogada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetJogada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSalvarArquivo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJogadaIA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJogadorDaVezEhIA() {
+		fail("Not yet implemented");
+	}
+
+}
